0ec63457db92ff3b6b4ef5973d5010552c0e769b
Santhosh P <santhoshperiyasamy25@gmail.com>
2026-08-09 21:32:29 +0530

    Add files via upload

---
+++ ifelse.py
@@ -0,0 +1,23 @@
+if(True):
+    print("Yes")
+else:
+    print("No")
+
+    #  o/p
+    #  Yes
+
+if(False):
+    print("Yes")
+else:
+    print("No")
+    
+    # o/p
+    #  No
+
+if(3>=5):
+    print("Yes")
+else:
+    print("No")
+    
+    #o/p
+    #No
