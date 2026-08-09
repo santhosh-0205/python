e6d76b5949f61e54152b3f88ce926cc44881bd5b
Santhosh P <santhoshperiyasamy25@gmail.com>
2026-08-09 21:33:37 +0530

    Add files via upload

---
+++ comparisen.py
@@ -0,0 +1,21 @@
+#if("santhosh"=="santhosh"):
+ #   print("Yes")
+#else:
+ #   print("No")
+
+#if("ananthi"=="santhosh"):
+ #   print("Yes")
+#else:
+ #   print("No")
+
+#csk="csk"
+#if(csk=="win"):
+ #   print("Chennai super kings")
+#else:
+    #print("Lose")
+
+Megana=input("Megana Died or Not?")
+if(Megana=="Died"):
+    print("Suriya meets Priya")
+else:
+    print("Suriya weds Megana")
