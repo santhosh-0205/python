4c2499c56dd1d268e8babcdbd7a0b10c521aaeaf
Santhosh P <santhoshperiyasamy25@gmail.com>
2026-08-10 21:42:09 +0530

    Add files via upload

---
+++ ifelseexersise.py
@@ -0,0 +1,18 @@
+'''mark=int(input())
+if(mark>35):
+    print("Pass")
+else:
+   print("Fail")'''
+
+'''income=int(input("Income:"))
+if(income>7000):
+    print("Scolarship is not Eligable")
+else:
+    print("Scolarship is  Eligable")'''
+
+'''a=int(input())
+if(a%3&5==0):
+    print("The number is divisible by 3 and 5")
+else:
+    print("The number is not divisible by 3 and 5")'''
+
